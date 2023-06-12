1710fc6d53f243594662cd1e463582c20d369d71
Heesang <80827042+Heeesang@users.noreply.github.com>
2023-06-12 23:22:55 +0900

    project setting

--- ZOO-iOS/MainViewController.swift
+++ ZOO-iOS/MainViewController.swift
@@ -11,9 +11,6 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .red
     }
-
-
 }
-
--- ZOO-iOS/SceneDelegate.swift
+++ ZOO-iOS/SceneDelegate.swift
@@ -11,13 +11,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
         let mainViewController = MainViewController()
-        
+
         let navigationController = UINavigationController(rootViewController: mainViewController)
         navigationController.navigationBar.isHidden = true
         window?.rootViewController = navigationController
@@ -51,7 +50,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
